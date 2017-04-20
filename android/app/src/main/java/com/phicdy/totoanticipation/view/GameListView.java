b557e975f2cd47736c9baa1f05a292cab5246866
phicdy <phicdy@gmail.com>
2017-04-20 19:56:03 +0800

    Fetch game list in onCreate by Retrofit

---
+++ android/app/src/main/java/com/phicdy/totoanticipation/view/GameListView.java
@@ -0,0 +1,11 @@
+package com.phicdy.totoanticipation.view;
+
+import android.support.annotation.NonNull;
+
+import com.phicdy.totoanticipation.model.Game;
+
+import java.util.ArrayList;
+
+public interface GameListView {
+    void initListBy(@NonNull ArrayList<Game> games);
+}
