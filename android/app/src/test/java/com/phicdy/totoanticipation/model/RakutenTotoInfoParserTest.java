61e66aeb9cc23467de9a8d0bcf515a7c5dfb8bd3
phicdy <phicdy@gmail.com>
2017-04-29 14:07:13 +0800

    Revert to shrunk team name in game list

--- android/app/src/test/java/com/phicdy/totoanticipation/model/RakutenTotoInfoParserTest.java
+++ android/app/src/test/java/com/phicdy/totoanticipation/model/RakutenTotoInfoParserTest.java
@@ -12,19 +12,19 @@ public class RakutenTotoInfoParserTest {
     @Test
     public void gameListReturns() {
         ArrayList<Game> expectedGames = new ArrayList<>();
-        expectedGames.add(new Game("浦和レッズ", "北海道コンサドーレ札幌"));
-        expectedGames.add(new Game("ヴァンフォーレ甲府", "セレッソ大阪"));
-        expectedGames.add(new Game("サンフレッチェ広島", "ベガルタ仙台"));
-        expectedGames.add(new Game("鹿島アントラーズ", "ジュビロ磐田"));
-        expectedGames.add(new Game("柏レイソル", "横浜F・マリノス"));
-        expectedGames.add(new Game("アルビレックス新潟", "FC東京"));
-        expectedGames.add(new Game("サガン鳥栖", "ヴィッセル神戸"));
-        expectedGames.add(new Game("名古屋グランパス", "レノファ山口FC"));
-        expectedGames.add(new Game("横浜FC", "ジェフユナイテッド千葉"));
-        expectedGames.add(new Game("京都サンガF.C.", "松本山雅FC"));
-        expectedGames.add(new Game("愛媛FC", "V・ファーレン長崎"));
-        expectedGames.add(new Game("東京ヴェルディ", "ザスパクサツ群馬"));
-        expectedGames.add(new Game("FC町田ゼルビア", "徳島ヴォルティス"));
+        expectedGames.add(new Game("浦和", "札幌"));
+        expectedGames.add(new Game("甲府", "Ｃ大阪"));
+        expectedGames.add(new Game("広島", "仙台"));
+        expectedGames.add(new Game("鹿島", "磐田"));
+        expectedGames.add(new Game("柏", "横浜Ｍ"));
+        expectedGames.add(new Game("新潟", "Ｆ東京"));
+        expectedGames.add(new Game("鳥栖", "神戸"));
+        expectedGames.add(new Game("名古屋", "山口"));
+        expectedGames.add(new Game("横浜Ｃ", "千葉"));
+        expectedGames.add(new Game("京都", "松本"));
+        expectedGames.add(new Game("愛媛", "長崎"));
+        expectedGames.add(new Game("東京Ｖ", "群馬"));
+        expectedGames.add(new Game("町田", "徳島"));
         RakutenTotoInfoParser parser = new RakutenTotoInfoParser();
         ArrayList<Game> actualGames = parser.games(TestRakutenTotoInfoPage.text);
         for (int i = 0; i < actualGames.size(); i++) {
