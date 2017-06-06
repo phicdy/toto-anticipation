b553325a4bb5646f0e5f7aeddedbf4f08d9fd890
phicdy <phicdy@gmail.com>
2017-06-06 21:27:16 +0900

    Set context in constructor

--- android/app/src/main/java/com/phicdy/totoanticipation/model/scheduler/DeadlineAlarm.java
+++ android/app/src/main/java/com/phicdy/totoanticipation/model/scheduler/DeadlineAlarm.java
@@ -11,14 +11,27 @@ import java.util.Date;
 
 public class DeadlineAlarm {
 
+    private final Context context;
     static final String ACTION = "deadlineAlarm";
 
-    public void set(@NonNull Context context, @NonNull Date date) {
-        Intent i = new Intent(context, DeadlineAlarmReceiver.class);
-        i.setAction(ACTION);
-        PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+    public DeadlineAlarm(@NonNull Context context) {
+        this.context = context;
+    }
+
+    public void set(@NonNull Date date) {
         AlarmManager alm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Log.d("Toto alarm", "Set alarm : " + date.toString());
-        alm.set(AlarmManager.RTC_WAKEUP, date.getTime(), pi);
+        alm.set(AlarmManager.RTC_WAKEUP, date.getTime(), generateIntent());
+    }
+
+    public void cancel() {
+        AlarmManager alm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alm.cancel(generateIntent());
+    }
+
+    private PendingIntent generateIntent() {
+        Intent i = new Intent(context, DeadlineAlarmReceiver.class);
+        i.setAction(ACTION);
+        return PendingIntent.getBroadcast(context, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 }
