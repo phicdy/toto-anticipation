b0aa35e16a8bdfac7ec844d57742ccd937556c2d
phicdy <phicdy@gmail.com>
2017-06-06 21:28:54 +0900

    Add activity for license

---
+++ android/app/src/main/java/com/phicdy/totoanticipation/view/activity/LicenseActivity.java
@@ -0,0 +1,15 @@
+package com.phicdy.totoanticipation.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.phicdy.totoanticipation.R;
+
+public class LicenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_license);
+    }
+}
