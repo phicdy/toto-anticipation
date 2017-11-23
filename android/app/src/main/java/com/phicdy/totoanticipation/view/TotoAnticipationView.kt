e28622512da544d47bc12e8490229fe26fd069c2
phicdy <phicdy@gmail.com>
2017-11-23 22:18:45 +0900

    Buy UI: Click buy now button in toto top page and check checkbox from user anticipation automatically

--- android/app/src/main/java/com/phicdy/totoanticipation/view/TotoAnticipationView.kt
+++ android/app/src/main/java/com/phicdy/totoanticipation/view/TotoAnticipationView.kt
@@ -3,4 +3,5 @@ package com.phicdy.totoanticipation.view
 interface TotoAnticipationView {
     fun initListener()
     fun loadUrl(url: String)
+    fun exec(javaScript: String)
 }
--- android/app/src/main/java/com/phicdy/totoanticipation/view/activity/TotoAnticipationActivity.java
+++ android/app/src/main/java/com/phicdy/totoanticipation/view/activity/TotoAnticipationActivity.java
@@ -11,10 +11,11 @@ import android.webkit.WebViewClient;
 
 import com.phicdy.totoanticipation.BuildConfig;
 import com.phicdy.totoanticipation.R;
+import com.phicdy.totoanticipation.model.storage.GameListStorageImpl;
+import com.phicdy.totoanticipation.presenter.TotoAnticipationPresenter;
 import com.phicdy.totoanticipation.view.TotoAnticipationView;
 
 import org.jetbrains.annotations.NotNull;
-
 
 public class TotoAnticipationActivity extends AppCompatActivity implements TotoAnticipationView {
 
@@ -31,7 +32,6 @@ public class TotoAnticipationActivity extends AppCompatActivity implements TotoA
         presenter = new TotoAnticipationPresenter(totoNum);
         presenter.setView(this);
         presenter.onCreate();
-
     }
 
     @Override
@@ -66,11 +66,26 @@ public class TotoAnticipationActivity extends AppCompatActivity implements TotoA
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
                 return super.shouldOverrideUrlLoading(view, request);
             }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                GameListStorageImpl storage = new GameListStorageImpl(TotoAnticipationActivity.this);
+                presenter.onPageFinished(url, storage.list(totoNum));
+            }
         });
     }
 
     @Override
     public void loadUrl(@NotNull String url) {
         webView.loadUrl(url);
+    }
+
+    @Override
+    public void exec(@NotNull String javaScript) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            webView.evaluateJavascript(javaScript, null);
+        } else {
+            webView.loadUrl("javascript:" + javaScript);
+        }
     }
 }
