9222f32954d77f15c2e71d0b512b77f3332b36a7
phicdy <phicdy@gmail.com>
2019-07-24 22:03:20 +0900

    DI: Add base component

--- android/app/src/main/java/com/phicdy/totoanticipation/TotoAnticipationApplication.kt
+++ android/app/src/main/java/com/phicdy/totoanticipation/TotoAnticipationApplication.kt
@@ -1,9 +1,13 @@
 package com.phicdy.totoanticipation
 
-import android.app.Application
+import com.phicdy.totoanticipation.di.DaggerAppComponent
 import com.phicdy.totoanticipation.model.notification.NotificationChannelFactory
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
 
-class TotoAnticipationApplication: Application() {
+class TotoAnticipationApplication : DaggerApplication() {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
+            DaggerAppComponent.factory().create(this)
 
     override fun onCreate() {
         super.onCreate()
---
+++ android/app/src/main/java/com/phicdy/totoanticipation/di/AppComponent.kt
@@ -0,0 +1,19 @@
+package com.phicdy.totoanticipation.di
+
+import com.phicdy.totoanticipation.TotoAnticipationApplication
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+import javax.inject.Singleton
+
+
+@Singleton
+@Component(
+        modules = [
+            AndroidInjectionModule::class
+        ]
+)
+interface AppComponent : AndroidInjector<TotoAnticipationApplication> {
+    @Component.Factory
+    abstract class Factory : AndroidInjector.Factory<TotoAnticipationApplication>
+}
