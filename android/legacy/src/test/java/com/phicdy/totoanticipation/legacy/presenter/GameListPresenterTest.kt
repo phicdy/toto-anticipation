d61680775b9706b3c7dff137ad2485192e67d55a
phicdy <phicdy@gmail.com>
2019-10-03 20:56:00 +0900

    Game List: Add test

--- android/legacy/src/test/java/com/phicdy/totoanticipation/legacy/presenter/GameListPresenterTest.kt
+++ android/legacy/src/test/java/com/phicdy/totoanticipation/legacy/presenter/GameListPresenterTest.kt
@@ -20,13 +20,20 @@ import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
+import io.mockk.verifyOrder
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import org.junit.experimental.theories.DataPoints
+import org.junit.experimental.theories.FromDataPoints
+import org.junit.experimental.theories.Theories
+import org.junit.experimental.theories.Theory
+import org.junit.runner.RunWith
 import java.util.Calendar
 import java.util.Date
 
+@RunWith(Theories::class)
 class GameListPresenterTest {
 
     private lateinit var presenter: GameListPresenter
@@ -36,6 +43,12 @@ class GameListPresenterTest {
     private lateinit var alarm: DeadlineAlarm
     private lateinit var jLeagueRepository: JLeagueRepository
     private lateinit var rakutenTotoRepository: RakutenTotoRepository
+
+    companion object {
+        @DataPoints("outOfIndex")
+        @JvmField
+        var outOfIndex = listOf(-1, 2)
+    }
 
     @Before
     fun setup() {
@@ -269,5 +282,183 @@ class GameListPresenterTest {
         presenter.onCreate()
         assertEquals(1, games[0].homeRanking)
         assertEquals(2, games[0].awayRanking)
+    }
+
+    @Test
+    fun `when home radio button is clicked then change anticipation and store`() = runBlocking {
+        val now = Calendar.getInstance().apply {
+            set(2019, 0, 1, 12, 34)
+        }.time
+        val toto = Toto("1", now)
+        val games = listOf(Game("home", "away"))
+        mockTotoReturns(toto, games)
+
+        presenter.onHomeRadioButtonClicked(0, true)
+        verifyOrder {
+            // In setup
+            storage.list("1")
+            storage.store(toto, games)
+
+            // Actual verification
+            storage.store(toto, games)
+        }
+        assertEquals(Game.Anticipation.HOME, games[0].anticipation)
+    }
+
+    @Theory
+    fun `when out of index home radio button is clicked then do nothing`(
+            @FromDataPoints("outOfIndex") outOfIndex: Int
+    ) = runBlocking {
+        val now = Calendar.getInstance().apply {
+            set(2019, 0, 1, 12, 34)
+        }.time
+        val toto = Toto("1", now)
+        val games = listOf(Game("home", "away"))
+        mockTotoReturns(toto, games)
+
+        presenter.onHomeRadioButtonClicked(outOfIndex, true)
+        // Do not call store() twice, setup only
+        verifyOrder {
+            storage.list("1")
+            storage.store(toto, games)
+        }
+    }
+
+    @Test
+    fun `when away radio button is clicked then change anticipation and store`() = runBlocking {
+        val now = Calendar.getInstance().apply {
+            set(2019, 0, 1, 12, 34)
+        }.time
+        val toto = Toto("1", now)
+        val games = listOf(Game("home", "away"))
+        mockTotoReturns(toto, games)
+
+        presenter.onAwayRadioButtonClicked(0, true)
+        verifyOrder {
+            // In setup
+            storage.list("1")
+            storage.store(toto, games)
+
+            // Actual verification
+            storage.store(toto, games)
+        }
+        assertEquals(Game.Anticipation.AWAY, games[0].anticipation)
+    }
+
+    @Test
+    fun `when draw radio button is clicked then change anticipation and store`() = runBlocking {
+        val now = Calendar.getInstance().apply {
+            set(2019, 0, 1, 12, 34)
+        }.time
+        val toto = Toto("1", now)
+        val games = listOf(Game("home", "away"))
+        mockTotoReturns(toto, games)
+
+        presenter.onDrawRadioButtonClicked(0, true)
+        verifyOrder {
+            // In setup
+            storage.list("1")
+            storage.store(toto, games)
+
+            // Actual verification
+            storage.store(toto, games)
+        }
+        assertEquals(Game.Anticipation.DRAW, games[0].anticipation)
+    }
+
+    @Test
+    fun `when get game at the position then return the game`() = runBlocking {
+        val now = Calendar.getInstance().apply {
+            set(2019, 0, 1, 12, 34)
+        }.time
+        val toto = Toto("1", now)
+        val game = Game("home", "away")
+        val games = listOf(game)
+        mockTotoReturns(toto, games)
+
+        val firstGame = presenter.gameAt(0)
+        assertEquals(game, firstGame)
+    }
+
+    @Test(expected = IndexOutOfBoundsException::class)
+    fun `when get out of index game then throw IndexOutOfBoundsException`() = runBlocking {
+        val now = Calendar.getInstance().apply {
+            set(2019, 0, 1, 12, 34)
+        }.time
+        val toto = Toto("1", now)
+        val game = Game("home", "away")
+        val games = listOf(game)
+        mockTotoReturns(toto, games)
+
+        presenter.gameAt(-1)
+        Unit
+    }
+
+    @Test
+    fun `when get game size then return the size`() = runBlocking {
+        val now = Calendar.getInstance().apply {
+            set(2019, 0, 1, 12, 34)
+        }.time
+        val toto = Toto("1", now)
+        val game = Game("home", "away")
+        val games = listOf(game)
+        mockTotoReturns(toto, games)
+
+        assertEquals(1, presenter.gameSize())
+    }
+
+    @Test
+    fun `when fab is clicked then store and go to anticipation activity`() = runBlocking {
+        val now = Calendar.getInstance().apply {
+            set(2019, 0, 1, 12, 34)
+        }.time
+        val toto = Toto("1", now)
+        val game = Game("home", "away")
+        val games = listOf(game)
+        mockTotoReturns(toto, games)
+
+        presenter.onFabClicked()
+        verify {
+            storage.store(toto, games)
+            view.startTotoAnticipationActivity("1")
+        }
+    }
+
+    @Test
+    fun `when option setting is clicked then go to option`() {
+        presenter.onOptionsSettingSelected()
+        verify { view.goToSetting() }
+    }
+
+    @Test
+    fun `when finish anticipation then stop and store`() = runBlocking {
+        val now = Calendar.getInstance().apply {
+            set(2019, 0, 1, 12, 34)
+        }.time
+        val toto = Toto("1", now)
+        val game = Game("home", "away")
+        val games = listOf(game)
+        mockTotoReturns(toto, games)
+
+        presenter.finishAnticipation()
+        verify {
+            view.stopProgress()
+            view.showAnticipationFinish()
+            view.notifyDataSetChanged()
+            storage.store(toto, games)
+        }
+    }
+
+    private suspend fun mockTotoReturns(toto: Toto, games: List<Game>) {
+        coEvery { rakutenTotoRepository.fetchLatestToto() } returns toto
+        every { settingStorage.isDeadlineNotify } returns false
+        every { storage.store(toto, games) } just Runs
+        every { settingStorage.isPrivacyPolicyAccepted } returns true
+        every { storage.list("1") } returns emptyList()
+        coEvery { jLeagueRepository.fetchJ1Ranking() } returns emptyList()
+        coEvery { jLeagueRepository.fetchJ2Ranking() } returns emptyList()
+        coEvery { jLeagueRepository.fetchJ3Ranking() } returns emptyList()
+        coEvery { rakutenTotoRepository.fetchTotoInfo(TotoNumber("1")) } returns TotoInfo(games, Deadline("12:34"))
+        presenter.onCreate()
     }
 }
