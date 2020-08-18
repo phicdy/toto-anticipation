9a3b227f85fa6a922b9bbcab97b07db86a835ebc
Keisuke Yamaguchi <phicdy@gmail.com>
2020-08-18 20:31:27 +0900

    License: Move fragment

--- android/feature_license/src/main/java/com/phicdy/totoanticipation/feature/license/LicenseFragment.kt
+++ android/feature_license/src/main/java/com/phicdy/totoanticipation/feature/license/LicenseFragment.kt
@@ -1,4 +1,4 @@
-package com.phicdy.totoanticipation.legacy.view.fragment
+package com.phicdy.totoanticipation.feature.license
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,7 +8,6 @@ import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import com.phicdy.totoanticipation.legacy.R
 
 class LicenseFragment : Fragment() {
 
