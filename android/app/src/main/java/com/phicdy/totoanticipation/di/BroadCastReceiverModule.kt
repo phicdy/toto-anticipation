9cc4b261a67b6ee5d26ca84861481b94e8373228
Keisuke Yamaguchi <phicdy@gmail.com>
2021-07-10 16:51:34 +0900

    DI: Add DeadlineAlarmReceiver

--- android/app/src/main/java/com/phicdy/totoanticipation/di/BroadCastReceiverModule.kt
+++ android/app/src/main/java/com/phicdy/totoanticipation/di/BroadCastReceiverModule.kt
@@ -1,6 +1,7 @@
 package com.phicdy.totoanticipation.di
 
 import com.phicdy.totoanticipation.scheduler.BootReceiver
+import com.phicdy.totoanticipation.scheduler.DeadlineAlarmReceiver
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -9,4 +10,7 @@ interface BroadCastReceiverModule {
 
     @ContributesAndroidInjector
     fun contributeBootReceiver(): BootReceiver
+
+    @ContributesAndroidInjector
+    fun contributeDeadlineAlarmReceiver(): DeadlineAlarmReceiver
 }
--- android/scheduler/src/main/java/com/phicdy/totoanticipation/scheduler/DeadlineAlarmReceiver.kt
+++ android/scheduler/src/main/java/com/phicdy/totoanticipation/scheduler/DeadlineAlarmReceiver.kt
@@ -1,18 +1,18 @@
 package com.phicdy.totoanticipation.scheduler
 
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.text.TextUtils
-
+import dagger.android.DaggerBroadcastReceiver
 import javax.inject.Inject
 
-class DeadlineAlarmReceiver : BroadcastReceiver() {
+class DeadlineAlarmReceiver : DaggerBroadcastReceiver() {
 
     @Inject
     lateinit var deadlineNotification: DeadlineNotification
 
     override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
         if (context == null || intent == null || TextUtils.isEmpty(intent.action) ||
                 intent.action != DeadlineAlarm.ACTION)
             return
